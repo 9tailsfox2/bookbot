8bdc0fe525b1257d4f05d1644cadeb0a26b98c21
9tailsfox2 <alexsoria1994@yahoo.com>
2025-05-14 20:48:08 -0400

    altered code to be able to reference any book

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+import sys
 from stats import word_count
 from stats import characters
 from stats import character_sort
+if len(sys.argv) < 2:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+filepath = sys.argv[1]
 def get_book_text(filepath):
 	with open(filepath) as f:
 		text = f.read()
 	return text
 
 def main():
-	text = get_book_text("books/frankenstein.txt")
+	text = get_book_text(filepath)
 	num_words = word_count(text)
 	char_counts = characters(text)
 	sorted_chars = character_sort(char_counts)
 	print ("============ BOOKBOT ============")
-	print (f"Analyzing book found at books/frankenstein.txt...")
+	print (f"Analyzing book found at {filepath}...")
 	print ("----------- Word Count ----------")
 	print (f"Found {num_words} total words")
 	print ("--------- Character Count -------")
